c74621b3ffc120b7e638d7e5d10e7a9a9e9cf4df
danialdvdvd@gmail.com <mellow.13821382@gmail.com>
2023-01-01 23:37:04 +0330

    write new tests

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -34,27 +34,66 @@ describe ("App e2e", () => {
     });
 
     describe("Auth", () => {
+        // this dto is used in signin route too, no problem with it
+        const dto: signupDto =  {
+            email: "danialdvdvd@gmail.com",
+            password: "1234",
+            username: "danial"
+        }
         describe('sign up', () => {
-            it("should signup", () => {
-                const dto: signupDto=  {
-                    email: "danialdvdvd@gmail.com",
-                    password: "1234",
-                    username: "danial"
-                }
-                return pactum.spec().post('/auth/signup').withBody(dto).expectStatus(201)
+            it("should throw error when email is empty", () => {
+                return pactum.spec().post('/auth/signup').withBody({password: dto.password}).expectStatus(400)
+
+            })
+            it("should throw error when password is empty", () => {
+                return pactum.spec().post('/auth/signup').withBody({email: dto.email}).expectStatus(400)
 
             })
 
+            it("should throw error when username is empty", () => {
+                return pactum.spec().post('/auth/signup').withBody({email: dto.email, password: dto.password}).expectStatus(400)
+
+            })
+
+            it("should throw error when data is empty", () => {
+                return pactum.spec().post('/auth/signup').withBody({}).expectStatus(400)
+
+            })
+
+            it("should signup", () => {
+                return pactum.spec().post('/auth/signup').withBody(dto).expectStatus(201).stores('userId', 'id')
+                
+            })
+            
         })
         describe('sign in', () => {
+            it("should throw error when email is empty", () => {
+                return pactum.spec().post('/auth/signin').withBody({password: dto.password}).expectStatus(400)
 
+            })
+            it("should throw error when password is empty", () => {
+                return pactum.spec().post('/auth/signin').withBody({email: dto.email}).expectStatus(400)
+
+            })
+
+            it("should throw error when data is empty", () => {
+                return pactum.spec().post('/auth/signin').withBody({}).expectStatus(400)
+
+            })
+
+            it("should signin", () => {
+                return pactum.spec().post('/auth/signin').withBody(dto).expectStatus(200).stores('userAt', 'jwtToken')
+
+            })
 
         })
 
     })
     describe('User', () => {
         describe('Get user by id', () => {
-
+            it('should get user by id', () => {
+                return pactum.spec().get("/user/$S{userId}").withHeaders({Authorization: "bearer $S{userAt}"}).expectStatus(200)
+            })
         })
 
         describe('Edit user', () => {
