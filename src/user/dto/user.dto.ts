72f57343ec13bd9559db0fc4364e311fcc6d7cf0
danialdvdvd@gmail.com <mellow.13821382@gmail.com>
2023-01-02 15:05:52 +0330

    add update user dto

--- src/user/dto/user.dto.ts
+++ src/user/dto/user.dto.ts
@@ -1,6 +1,39 @@
-import { IsEmail, IsNotEmpty, IsNumber, IsString } from "class-validator";
+import { IsEmail, IsNotEmpty, IsNotEmptyObject, IsNumber, IsObject, IsOptional, IsString, ValidateNested } from "class-validator";
+import { Type } from 'class-transformer';
 
-export class userDto { 
+
+
+// private class
+export class UpdateUserfieldsDto {
+    @IsOptional()
+    @IsEmail()
+    email: string;
+    
+    @IsOptional()
     @IsString()
+    username: string;
+    
+    @IsOptional()
+    @IsString()
+    firstName: string;
+    
+    @IsOptional()
+    @IsString()
+    lastName: string;
+
+}
+
+
+export class UpdateUserDto { 
+    @IsNotEmpty()
+    @IsNumber()
     id: number;
+
+    @IsObject()
+    @IsNotEmptyObject()
+    @ValidateNested()
+    @Type(() => UpdateUserfieldsDto)
+    fields: UpdateUserfieldsDto;
+
+
 }
